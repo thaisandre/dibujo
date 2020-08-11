b3e446d7522d17cdc809a24806510d422bf215dd
Caio Bugorin <caio.bugorin@caelum.com.br>
2020-08-11 15:12:52 -0300

    extraindo logica para o enum - (testes nao rodando)

---
+++ src/main/java/dibujo/Command.java
@@ -0,0 +1,110 @@
+package dibujo;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import static java.util.regex.Pattern.compile;
+
+public enum Command {
+    CREATE_CANVAS("C", compile("^C (\\d+) (\\d+)$")) {
+        public void execute(Canvas canvas, String line) {
+            Matcher matcher = this.getPattern().matcher(line);
+            if (matcher.find()) {
+                int width = Integer.parseInt(matcher.group(1));
+                int height = Integer.parseInt(matcher.group(2));
+
+                canvas = new Canvas(width, height);
+            } else {
+                throw new RuntimeException("Invalid parameters for the create new canvas command. Should be: C <width> <height>");
+            }
+        }
+    },
+    DRAW_LINE("L", compile("^L (\\d+) (\\d+) (\\d+) (\\d+)$")) {
+        public void execute(Canvas canvas, String line) {
+            if (canvas == null) {
+                throw new RuntimeException("No canvas. You should create a canvas before creating a new line.");
+            }
+            Matcher matcher = getPattern().matcher(line);
+            if (matcher.find()) {
+                int startingX = Integer.parseInt(matcher.group(1));
+                int startingY = Integer.parseInt(matcher.group(2));
+                int endingX = Integer.parseInt(matcher.group(3));
+                int endingY = Integer.parseInt(matcher.group(4));
+
+                canvas.createNewLine(startingX, startingY, endingX, endingY);
+            } else {
+                throw new RuntimeException("Invalid parameters for the create new line command. Should be: L <starting x> <starting y> <ending x> <ending y>");
+            }
+        }
+    },
+    DRAW_RECTANGLE("R", compile("^R (\\d+) (\\d+) (\\d+) (\\d+)$")) {
+        public void execute(Canvas canvas, String line) {
+            if (canvas == null) {
+                throw new RuntimeException("No canvas. You should create a canvas before creating a new rectangle.");
+            }
+
+            Matcher matcher = getPattern().matcher(line);
+            if (matcher.find()) {
+                int upperLeftCornerX = Integer.parseInt(matcher.group(1));
+                int upperLeftCornerY = Integer.parseInt(matcher.group(2));
+                int lowerRightCornerX = Integer.parseInt(matcher.group(3));
+                int lowerRightCornerY = Integer.parseInt(matcher.group(4));
+
+                canvas.createNewRectangle(upperLeftCornerX, upperLeftCornerY, lowerRightCornerX, lowerRightCornerY);
+            } else {
+                throw new RuntimeException("Invalid parameters for the create new rectangle command. Should be: L <upper left corner x> <upper left corner y> <lower right corner x> <lower right corner y>");
+            }
+        }
+    },
+    FILL("B", compile("^B (\\d+) (\\d+) (\\w+)$")) {
+        public void execute(Canvas canvas, String line) {
+            if (canvas == null) {
+                throw new RuntimeException("No canvas. You should create a canvas before filling it.");
+            }
+
+            Matcher matcher = getPattern().matcher(line);
+            if (matcher.find()) {
+                int startingX = Integer.parseInt(matcher.group(1));
+                int startingY = Integer.parseInt(matcher.group(2));
+                String colorCharacter = matcher.group(3);
+
+                Position startingPositionToFill = new Position(startingX, startingY);
+                canvas.fill(startingPositionToFill, colorCharacter);
+            } else {
+                throw new RuntimeException("Invalid parameters for the bucket fill command. Should be: B <starting x> <starting y> <color>");
+            }
+        }
+    },
+    EXIT("Q", compile("^Q")) {
+        public void execute(Canvas canvas, String line) {
+            System.out.println("Bye bye!");
+            System.exit(0);
+        }
+    };
+
+    private String startsWith;
+    private Pattern pattern;
+
+    Command(String startsWith, Pattern pattern) {
+        this.startsWith = startsWith;
+        this.pattern = pattern;
+    }
+
+    public boolean matches(String line) {
+        return this.pattern.matcher(line).matches();
+    }
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public static Optional<Command> findCommand(String line) {
+        return Stream.of(Command.values())
+                .filter(c -> line.startsWith(c.startsWith))
+                .findFirst();
+    }
+
+    public abstract void execute(Canvas canvas, String line);
+}
--- src/main/java/dibujo/Main.java
+++ src/main/java/dibujo/Main.java
@@ -2,6 +2,7 @@ package dibujo;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,17 +17,11 @@ public class Main {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 try {
-                    if (line.startsWith("C")) {
-                        createCanvas(line);
-                    } else if (line.startsWith("L")) {
-                        drawLine(line);
-                    } else if (line.startsWith("R")) {
-                        drawRetangle(line);
-                    } else if (line.startsWith("B")) {
-                        fill(line);
-                    } else if (line.startsWith("Q")) {
-                        exit(out);
-                    } else {
+                    Canvas canvas = null;
+                    Optional<Command> command = Command.findCommand(line);
+                    command.ifPresent(c -> c.execute(canvas, line));
+
+                    if(command.isEmpty()) {
                         err.println("Invalid command: " + line + "\n");
                     }
 
@@ -46,75 +41,7 @@ public class Main {
         }
     }
 
-    private void exit(PrintStream out) {
-        out.println("Bye bye!");
-        System.exit(0);
-    }
 
-    private void fill(String line) {
-        if (canvas == null) {
-            throw new RuntimeException("No canvas. You should create a canvas before filling it.");
-        }
-
-        Matcher matcher = Pattern.compile("^B (\\d+) (\\d+) (\\w+)$").matcher(line);
-        if (matcher.find()) {
-            int startingX = Integer.parseInt(matcher.group(1));
-            int startingY = Integer.parseInt(matcher.group(2));
-            String colorCharacter = matcher.group(3);
-
-            Position startingPositionToFill = new Position(startingX, startingY);
-            canvas.fill(startingPositionToFill, colorCharacter);
-        } else {
-            throw new RuntimeException("Invalid parameters for the bucket fill command. Should be: B <starting x> <starting y> <color>");
-        }
-    }
-
-    private void drawRetangle(String line) {
-        if (canvas == null) {
-            throw new RuntimeException("No canvas. You should create a canvas before creating a new rectangle.");
-        }
-
-        Matcher matcher = Pattern.compile("^R (\\d+) (\\d+) (\\d+) (\\d+)$").matcher(line);
-        if (matcher.find()) {
-            int upperLeftCornerX = Integer.parseInt(matcher.group(1));
-            int upperLeftCornerY = Integer.parseInt(matcher.group(2));
-            int lowerRightCornerX = Integer.parseInt(matcher.group(3));
-            int lowerRightCornerY = Integer.parseInt(matcher.group(4));
-
-            canvas.createNewRectangle(upperLeftCornerX, upperLeftCornerY, lowerRightCornerX, lowerRightCornerY);
-        } else {
-            throw new RuntimeException("Invalid parameters for the create new rectangle command. Should be: L <upper left corner x> <upper left corner y> <lower right corner x> <lower right corner y>");
-        }
-    }
-
-    private void drawLine(String line) {
-        if (canvas == null) {
-            throw new RuntimeException("No canvas. You should create a canvas before creating a new line.");
-        }
-        Matcher matcher = Pattern.compile("^L (\\d+) (\\d+) (\\d+) (\\d+)$").matcher(line);
-        if (matcher.find()) {
-            int startingX = Integer.parseInt(matcher.group(1));
-            int startingY = Integer.parseInt(matcher.group(2));
-            int endingX = Integer.parseInt(matcher.group(3));
-            int endingY = Integer.parseInt(matcher.group(4));
-
-            canvas.createNewLine(startingX, startingY, endingX, endingY);
-        } else {
-            throw new RuntimeException("Invalid parameters for the create new line command. Should be: L <starting x> <starting y> <ending x> <ending y>");
-        }
-    }
-
-    private void createCanvas(String line) {
-        Matcher matcher = Pattern.compile("^C (\\d+) (\\d+)$").matcher(line);
-        if (matcher.find()) {
-            int width = Integer.parseInt(matcher.group(1));
-            int height = Integer.parseInt(matcher.group(2));
-
-            canvas = new Canvas(width, height);
-        } else {
-            throw new RuntimeException("Invalid parameters for the create new canvas command. Should be: C <width> <height>");
-        }
-    }
 
     public static void main(String[] args) {
         new Main().run(System.in, System.out, System.err);
